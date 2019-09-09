a51b7be3a727a13ce217566507b90605260a7bd4
Yt-trium <cyril.meyer@etu.unistra.fr>
2019-09-09 04:53:11 +0200

    add SSG test file, there is still some bugs to catch

--- SSG/SSG.ino
+++ SSG/SSG.ino
@@ -2,6 +2,19 @@
 
 SSG ssg;
 
+/* Same as delay(d) (or delay(d-1) if d%2 == 1
+ * Make the builtin led blink on half the time of the delay
+ */
+void delay_blink(unsigned long d)
+{
+  unsigned long d_2 = d / 2;
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(d_2);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(d_2);
+}
+
 void setup() {
 unsigned int i;
   Serial.begin(115200);
@@ -10,44 +23,143 @@ unsigned int i;
 }
 
 void loop() {
-  unsigned int i, j;
+  unsigned int i;
+  
+  // 1 - Test Music
 
-  // tests all notes
-  for(i = 0; i < 10; ++i)
+  ssg.set_chan_level(0b1111, chanA | chanB | chanC);
+  ssg.set_chan_mixer_mute(chanA | chanB | chanC);
+  ssg.set_chan_mixer(true, false, chanA);
+  
+  for(i = 1; i < 6; ++i)
   {
-    
     ssg.set_chanA_frequency(C, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(D, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(E, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(F, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(G, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(A, i);
-    delay(500);
+    delay_blink(200);
     ssg.set_chanA_frequency(B, i);
-    delay(500);
-    
-    /*
-    Serial.println(pgm_read_float(&note_frequency[C][i]));
-    Serial.println(pgm_read_float(&note_frequency[D][i]));
-    Serial.println(pgm_read_float(&note_frequency[E][i]));
-    Serial.println(pgm_read_float(&note_frequency[F][i]));
-    Serial.println(pgm_read_float(&note_frequency[G][i]));
-    Serial.println(pgm_read_float(&note_frequency[A][i]));
-    Serial.println(pgm_read_float(&note_frequency[B][i]));
-    */
+    delay_blink(200);
   }
+  ssg.set_chan_mixer(true, false, chanA | chanB | chanC);
 
+  ssg.set_chanA_frequency(C, 3);
+  ssg.set_chanB_frequency(E, 4);
+  ssg.set_chanC_frequency(G, 5);
+  delay_blink(1000);
+  
+  ssg.set_chan_frequency(C, 4, chanA | chanB | chanC);
+  delay_blink(1000);
+  
+  ssg.set_chanA_frequency(C, 4);
+  ssg.set_chanB_frequency(E, 5);
+  ssg.set_chanC_frequency(G, 6);
+  delay_blink(1000);
+
+
+  // 2 - Noise and Mixer
+  ssg.set_noise_frequency(0b01111);
+  delay_blink(1000);
+  
+  ssg.set_chanA_mixer(false, true);
+  delay_blink(1000);
+  ssg.set_chanB_mixer(false, true);
+  delay_blink(1000);
+  ssg.set_chanC_mixer(false, true);
+  delay_blink(1000);
+  
+  ssg.set_noise_frequency(0b11111);
+  delay_blink(1000);
+
+  ssg.set_chan_mixer(false, false, chanA | chanB | chanC);
+  delay_blink(1000);
+  ssg.set_chanA_mixer(true, false);
+  ssg.set_chanB_mixer(false, false);
+  ssg.set_chanC_mixer(false, false);
+  delay_blink(1000);
+
+
+  // 3 - Test Level
+  
+  ssg.set_chanA_level(0b1111);
   ssg.set_chanA_frequency(A, 4);
-  delay(1000);
-  ssg.set_chanB_frequency(A, 5);
-  delay(1000);
-  ssg.set_chanC_frequency(A, 6);
-  delay(1000);
-  ssg.set_chan_frequency_null(chanA | chanB | chanC);
-  delay(1000);
+  delay_blink(1000);
+
+  for(i = 0; i < 0b1111; ++i)
+  {
+    ssg.set_chanA_level(i);
+    delay_blink(200);
+  }
+  
+  
+  // 4 - Test Envelope
+  
+  ssg.set_envelope_shape(true, false, true, false);
+  ssg.set_chanA_use_envelope();
+
+  ssg.set_envelope_frequency(32);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(64);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(128);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(256);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(512);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(1024);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(2048);
+  delay_blink(1000);
+  
+
+  ssg.set_envelope_shape(false, false, false, false);
+  ssg.set_chanA_use_envelope();
+
+  ssg.set_envelope_frequency(512);
+  delay_blink(1000);
+  
+  ssg.set_envelope_shape(false, true, false, false);
+  ssg.set_chanA_use_envelope();
+
+  ssg.set_envelope_frequency(512);
+  delay_blink(1000);
+
+
+  ssg.set_envelope_shape(true, false, false, false);
+  ssg.set_chanA_use_envelope();
+
+  ssg.set_envelope_frequency(32);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(64);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(128);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(256);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(512);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(1024);
+  delay_blink(1000);
+  
+  ssg.set_envelope_frequency(2048);
+  delay_blink(1000);
 }
